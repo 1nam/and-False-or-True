753fcde1ae33b0226faee4054f58f2322c3ec314
zeus <33592162+1nam@users.noreply.github.com>
2019-10-30 06:09:43 -0400

    Create false_true.py

---
+++ false_true.py
@@ -0,0 +1,15 @@
+# using and for False then use or for True.
+coder = True
+user = False
+knows_python = True
+cant_code = False
+
+if user and cant_code:   # using the and for false.
+    print("you are user and does not know python...")
+else:                    # so print this.
+    print("you are a coder and knows python!")
+
+if user or cant_code:   # using the or Makes it True.
+    print("you are a coder and knows python!")
+else:                    # so print this.
+    print("you are user and does not know python...")   
